14dede07c08691135505d58d39a3a8eed592e743
youngjerald239 <youngjerald239@gmail.com>
2022-08-20 13:24:33 -0500

    started setup for player controls

--- player.py
+++ player.py
@@ -6,4 +6,14 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((32,64))
         self.image.fill('red')
         self.rect = self.image.get_rect(topleft = pos)
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT]:
+            pass
+        elif keys[pygame.K_LEFT]:
+            pass
+        else:
+            pass
         
