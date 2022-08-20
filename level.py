54c770b536809efbe474b2562fc768719fb9489b
youngjerald239 <youngjerald239@gmail.com>
2022-08-19 21:42:33 -0500

    updated level.py to show row index

--- level.py
+++ level.py
@@ -4,7 +4,13 @@ from tiles import Tile
 class Level:
     def __init__(self,level_data,surface):
         self.display_surface = surface
-        self.level_data = level_data
+        self.setup_level(level_data)
+
+    def setup_level(self,layout):
+        self.tiles = pygame.sprite.Group()
+        for row_index, row in enumerate(layout):
+            print(row_index)
+            print(row)
 
     def run(self):
-        pass
+        self.tiles.draw(self.display_surface)
