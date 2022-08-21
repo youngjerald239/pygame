f7a674396caeb75495424d303eb29331780d5431
youngjerald239 <youngjerald239@gmail.com>
2022-08-20 22:06:36 -0500

    added jump function

--- player.py
+++ player.py
@@ -23,9 +23,15 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
+        if keys[pygame.K_SPACE]:
+            self.jump()
+
     def apply_gravity(self):
         self.direction.y += self.gravity
         self.rect.y += self.direction.y
+
+    def jump(self):
+        self.direction.y = self.jump_speed
 
     def update(self):
         self.get_input()
