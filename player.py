ca9ef0d1ac2c0ac0158b44cc2e0757465eaa9f5f
youngjerald239 <youngjerald239@gmail.com>
2022-08-21 20:29:36 -0500

    add flipped image for 'run' animations

--- player.py
+++ player.py
@@ -18,6 +18,7 @@ class Player(pygame.sprite.Sprite):
 
         # player status
         self.status = 'idle'
+        self.facing_right = True
 
     def import_character_assets(self):
         character_path = './graphics/character/'
@@ -35,15 +36,22 @@ class Player(pygame.sprite.Sprite):
         if self.frame_index >= len(animation):
             self.frame_index = 0
 
-        self.image = animation[int(self.frame_index)]
+        image = animation[int(self.frame_index)]    
+        if self.facing_right:
+            self.image = image
+        else:
+            flipped_image = pygame.transform.flip(image,True,False)
+            self.image = flipped_image
 
     def get_input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.facing_right = True
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.facing_right = False
         else:
             self.direction.x = 0
 
