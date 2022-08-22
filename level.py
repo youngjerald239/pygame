2ee71708b823f0a58ba650d8c0d223f0ad98e455
youngjerald239 <youngjerald239@gmail.com>
2022-08-22 13:12:32 -0500

    added dust jump and land particles

--- level.py
+++ level.py
@@ -2,6 +2,7 @@ import pygame
 from tiles import Tile
 from settings import tile_size, screen_width
 from player import Player
+from particles import ParticleEffect
 
 class Level:
     def __init__(self,level_data,surface):
@@ -11,6 +12,13 @@ class Level:
         self.setup_level(level_data)
         self.world_shift = 0
         self.current_x = 0
+
+        # dust 
+        self.dust_sprite = pygame.sprite.GroupSingle()
+
+    def create_jump_particles(self,pos):
+        jump_particle_sprite = ParticleEffect(pos,'jump')
+        self.dust_sprite.add(jump_particle_sprite)
 
     def setup_level(self,layout):
         self.tiles = pygame.sprite.Group()
@@ -25,7 +33,7 @@ class Level:
                     tile = Tile((x,y),tile_size)
                     self.tiles.add(tile)
                 if cell == 'P':                 # Tile info for the Player
-                    player_sprite = Player((x,y),self.display_surface)
+                    player_sprite = Player((x,y),self.display_surface,self.create_jump_particles)
                     self.player.add(player_sprite)
             #    print(f'{row_index},{col_index}:{cell}') for exact X value and row numbers
             # print(row_index) To view row data and number
@@ -93,6 +101,10 @@ class Level:
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
         self.scroll_x()
+
+        # dust particles
+        self.dust_sprite.update(self.world_shift)
+        self.dust_sprite.draw(self.display_surface)
 
         # Player
         self.player.update()
--- player.py
+++ player.py
@@ -3,7 +3,7 @@ import pygame
 from support import import_folder
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self,pos,surface):
+    def __init__(self,pos,surface,create_jump_particles):
         super().__init__()
         self.import_character_assets()      
         self.frame_index = 0
@@ -16,6 +16,7 @@ class Player(pygame.sprite.Sprite):
         self.dust_frame_index = 0
         self.dust_animation_speed = 0.15
         self.display_surface = surface
+        self.create_jump_particles = create_jump_particles
 
         # player movement
         self.direction = pygame.math.Vector2(0,0)         # Vector2 is a list that contains an x and a y value
@@ -101,6 +102,7 @@ class Player(pygame.sprite.Sprite):
 
         if keys[pygame.K_SPACE] and self.on_ground:
             self.jump()
+            self.create_jump_particles(self.rect.midbottom)
 
     def get_status(self):
         if self.direction.y < 0:
