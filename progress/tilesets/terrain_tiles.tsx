fdb9f930b8034bca311ca63dd81b3d15c116cc55
youngjerald239 <youngjerald239@gmail.com>
2022-08-22 19:39:45 -0500

    made changes to character model

--- progress/tilesets/terrain_tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.1" name="terrain_tiles" tilewidth="64" tileheight="64" tilecount="70" columns="14">
- <image source="../../graphics/terrain/terrain_tiles.png" width="928" height="320"/>
-</tileset>
